c37af924dc236cae025cccd3b89357dd722c2172
MahilJain <Mahiljee001@gmail.com>
2026-07-01 17:54:00 +0530

    Refactor code structure for improved readability and maintainability

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import 'remixicon/fonts/remixicon.css'
 
 
 function App() {
@@ -31,9 +32,22 @@ function App() {
       });
   });
 
+  useGSAP(() => {
+    const main = document.querySelector(".main");
+
+    main?.addEventListener("mousemove", function (e) {
+      const xMove = (e.clientX / window.innerWidth - 0.5) * 40;
+      gsap.to(".imagesdiv .text", {
+        x: `${xMove}%`
+      })
+
+    })
+
+  }, [showContent])
+
   return (
     <>
-      <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidde bg-[#000]">
+      <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
         <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
           <defs>
             <mask id="viMask">
@@ -63,12 +77,43 @@ function App() {
         </svg>
       </div>
       {showContent && (
-        <div className="main w-full">
-          <div className="landing w-full h-screen bg-black">
-            <div className='imagesdiv w-full h-screen'>
-              <img className = "absolute top-0 left-0 w-full h-full object-cover" src="./sky1.jpeg" alt="" />
-              <img className = "absolute top-0 left-0 w-full h-full object-cover" src="./image.png" alt="" />
-              <img className="absolute -bottom-[-5%] left-1/3 scale-[1.3]" src="./chr.png" alt="" style={{ width: '500px', height: 'auto' }} />
+        <div className="main w-full ">
+          <div className="landing overflow-hidden relative w-full h-screen bg-black">
+            <div className="navbar absolute top-0 left-0 z-[10] w-full py-10 px-10">
+              <div className="logo flex gap-7">
+                <div className="lines flex flex-col gap-[5px] ">
+                  <div className="line w-15 h-2 bg-white"></div>
+                  <div className="line w-8 h-2 bg-white"></div>
+                  <div className="line w-5 h-2 bg-white"></div>
+                </div>
+                <h3 className="text-4xl -mt-[8px] leading-none text-white">
+                  Rockstar
+                </h3>
+              </div>
+            </div>
+
+            <div className='imagesdiv relative overflow-hidden w-full h-screen'>
+              <img className="absolute top-0 left-0 w-full h-full object-cover" src="./sky1.png" alt="" />
+              <img className="absolute top-0 left-0 w-full h-full object-cover" src="./bg4.png" alt="" />
+
+              <div className="text text-white flex flex-col gap-3 absolute top-20 left-1/2 -translate-x-1/2 scale-[1.4] rotate-[-10deg]">
+                <h1 className="text-[9rem] leading-none -ml-40">grand</h1>
+                <h1 className="text-[9rem] leading-none ml-20">theft</h1>
+                <h1 className="text-[9rem] leading-none -ml-40">auto</h1>
+              </div>
+
+              <img className="absolute -bottom-[-2%] left-1/2 -translate-x-1/2 scale-[1.3]" src="./chr.png" alt="" style={{ width: '500px', height: 'auto' }} />
+
+
+            </div>
+            <div className="btmbar text-white absolute bottom-0 left-0 w-full py-15 px-10 bg-gradient-to-t from-black to-transparent">
+              <div className="flex gap-4 items-center">
+                <i className="text-4xl ri-arrow-down-line"></i>
+                <h3 className="text-xl font-[Helvetica_Now_Display]">Scroll Down</h3>
+              </div>
+              <img className='absolute h-[55px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[60px]'
+                src="./ps5.png"
+                alt="" />
             </div>
           </div>
         </div>
